50f82fde46da41f5dbb04e50a8b83efc24aeeb9f
Rovshana <rovshanarzayeva2001@gmail.com>
2022-05-23 19:48:19 +0400

    admin.html joi us table connected to the firabese

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,80 +1,86 @@
-$('#join-submit-btn').on('click', processForm);
-let failBox = $('#fail-box');
+$("#join-submit-btn").on("click", processForm);
+let failBox = $("#fail-box");
 
 function processForm(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let name = $('#join-us-personal-name').val();
-    let email = $('#join-us-personal-email').val();
+  let name = $("#join-us-personal-name").val();
+  let email = $("#join-us-personal-email").val();
 
-    if (!name) {
-        failBox.text('Name is empty!');
-        failBox.fadeIn(600);
-        failBox.fadeOut(2500);
-        failBox.addClass('alert-danger');
-        return;
-    }
-    if (!email) {
-        failBox.text('Email is empty!');
-        failBox.fadeIn(600);
-        failBox.fadeOut(2500);
-        failBox.addClass('alert-danger');
-        return;
-    }
-    if (!name && !email) {
-        failBox.text('Email & Name are empty!');
-        failBox.fadeIn(600);
-        failBox.fadeOut(2500);
-        failBox.addClass('alert-danger');
-        return;
-    }
-    if (name.length >= 20) {
-        failBox.text('The Name greater than 20 character!');
-        failBox.fadeIn(600);
-        failBox.fadeOut(2500);
-        failBox.addClass('alert-danger');
-        return;
-    }
-    if (email.length >= 35) {
-        failBox.text('The Email greater than 35 Character');
-        failBox.fadeIn(600);
-        failBox.fadeOut(2500);
-        failBox.addClass('alert-danger');
-        return;
-    }
-    if (email.indexOf('@') === -1) {
-        failBox.text('Invalid Email! Example: Jeff@gmail.com');
-        failBox.fadeIn(500);
-        failBox.fadeOut(3000);
-        failBox.addClass('alert-danger');
-        return;
-    }
-    writeFirebase(name, email);
-}
-
-
-function writeFirebase(name, email) {
-
-    //...wrirte DB
-
-    failBox.removeClass('alert-danger');
-    failBox.addClass('alert-primary');
-    failBox.text('Thanks! Your informations successfully saved');
+  if (!name) {
+    failBox.text("Name is empty!");
+    failBox.fadeIn(600);
+    failBox.fadeOut(2500);
+    failBox.addClass("alert-danger");
+    return;
+  }
+  if (!email) {
+    failBox.text("Email is empty!");
+    failBox.fadeIn(600);
+    failBox.fadeOut(2500);
+    failBox.addClass("alert-danger");
+    return;
+  }
+  if (!name && !email) {
+    failBox.text("Email & Name are empty!");
+    failBox.fadeIn(600);
+    failBox.fadeOut(2500);
+    failBox.addClass("alert-danger");
+    return;
+  }
+  if (name.length >= 20) {
+    failBox.text("The Name greater than 20 character!");
+    failBox.fadeIn(600);
+    failBox.fadeOut(2500);
+    failBox.addClass("alert-danger");
+    return;
+  }
+  if (email.length >= 35) {
+    failBox.text("The Email greater than 35 Character");
+    failBox.fadeIn(600);
+    failBox.fadeOut(2500);
+    failBox.addClass("alert-danger");
+    return;
+  }
+  if (email.indexOf("@") === -1) {
+    failBox.text("Invalid Email! Example: Jeff@gmail.com");
     failBox.fadeIn(500);
     failBox.fadeOut(3000);
-    setTimeout(() => $('.modal').click(), 2800); //close join-us modal
+    failBox.addClass("alert-danger");
     return;
+  }
+  writeFirebase(name, email);
+  name = $("#join-us-personal-name").val("");
+  email = $("#join-us-personal-email").val("");
 }
 
-$('#hamburger-btn').on('click', () => {
-    $('.mobile-view').show(500).css('display', 'flex');
+database.ref("/join_us");
+let joinUsBranch = database.ref("/join_us");
+
+function writeFirebase(name, email) {
+  joinUsBranch.push().set({ name, email });
+
+  //...wrirte DB
+  console.log(name, email);
+
+  failBox.removeClass("alert-danger");
+  failBox.addClass("alert-primary");
+  failBox.text("Thanks! Your informations successfully saved");
+  failBox.fadeIn(500);
+  failBox.fadeOut(3000);
+  setTimeout(() => $(".modal").click(), 2800); //close join-us modal
+  return;
+}
+
+$("#hamburger-btn").on("click", () => {
+  $(".mobile-view").show(500).css("display", "flex");
 });
 
-$('#close-mobile-view').on('click', () => {
-    $('.mobile-view').hide(500)
+$("#close-mobile-view").on("click", () => {
+  $(".mobile-view").hide(500);
 });
 
 $(function () {
-    let currentLink = `header nav a[href="./${location.pathname.split("/")[1]}"]`;
-    $(currentLink).addClass('current');
+  let currentLink = `header nav a[href="./${location.pathname.split("/")[1]}"]`;
+  $(currentLink).addClass("current");
 });
