75aed6b604baebfde16acc774cb95cc79efeb9a7
Fatali Fataliyev <fataliyev999@gmail.com>
2022-06-02 08:40:12 +0400

    about store section completed

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,6 +1,33 @@
-let aboutBranch = database.ref('/about');
-
-aboutBranch.on("value", function (snap) {
-    console.log(snap.val());
+$(document).ready(function () {
+    let aboutBranch = database.ref('/about_store');
+    let aboutStoreInfo;
+    aboutBranch.on("value", function (snap) {
+        aboutStoreInfo = snap.val();
+    });
+    setTimeout(printDOM, 1300);
+    
+    function printDOM() {
+        console.log(aboutStoreInfo);
+        $('#about-container').html(
+            `
+            <div id="about-title">
+                ${aboutStoreInfo.bookTitle}
+            </div>
+                <div id="about-boxes">
+        
+            <div id="content">
+              <div id="about-title-mobile">
+              ${aboutStoreInfo.bookTitle}
+              </div>
+                ${aboutStoreInfo.bookDescription}
+            </div>
+            <div id="image">
+                <img src="${aboutStoreInfo.bookImageUrl}" alt="About Image">
+            </div>
+        </div>        
+        
+        `
+        );
+    }
 });
 
